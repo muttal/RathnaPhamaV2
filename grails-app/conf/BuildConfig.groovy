af60e097e02e276ffcc3e480a065b13a997c2ac0
naveenrajamoorthy <naveenrajamoorthy.r@gmail.com>
2016-08-20 13:50:02 -0400

    Commiting postgres changes

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -51,7 +51,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
          runtime 'mysql:mysql-connector-java:5.1.29'
-        // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
+         runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
 //        test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
     }
 
@@ -63,6 +63,8 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":asset-pipeline:2.1.5"
+        compile ":heroku:1.0.1"
+        compile ":cloud-support:1.0.11"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.8.1" // or ":hibernate:3.6.10.18"
